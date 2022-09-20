c575549bae757b6826563783139f7c933cea60ef
Ahmed Safwat <ahmad.safwat@inspirejo.com>
2022-09-20 11:27:29 +0200

    add min length validation to plain text field

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,12 @@
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class langEnum(str, Enum):
     AR = 'AR'
     EN = 'EN'
     
 class EncryptRq(BaseModel):
-    plainText: str
+    plainText: str = Field(min_length=1)
     language: langEnum
     
 class EncryptCaesarRq(EncryptRq):
